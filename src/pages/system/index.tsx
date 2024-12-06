8a95234bc82dc129e0ca17b669b70be4c04da6d3
sparkbrains <info@sparkbrains.in>
2024-12-06 18:53:54 +0530

    changed the default page to ticker during initial load

--- src/pages/system/index.tsx
+++ src/pages/system/index.tsx
@@ -9,7 +9,7 @@ import Risk from "../../component/system/Risk";
 import Contracts from "../../component/system/Contracts";
 
 export default function System() {
-  const [tab, setTab] = useState(4);
+  const [tab, setTab] = useState(0);
 
   return (
     <AppLayout>
@@ -19,9 +19,9 @@ export default function System() {
         </Card>
         {tab === 0 && <Ticker handleTabChange={() => setTab(1)} />}
         {tab === 1 && <Timing handleTabChange={() => setTab(2)} />}
-        {tab == 2 && <Range handleTabChange={() => setTab(3)} />}
-        {tab == 3 && <Risk handleTabChange={() => setTab(4)} />}
-        {tab == 4 && <Contracts handleTabChange={() => setTab(5)} />}
+        {tab === 2 && <Range handleTabChange={() => setTab(3)} />}
+        {tab === 3 && <Risk handleTabChange={() => setTab(4)} />}
+        {tab === 4 && <Contracts handleTabChange={() => setTab(5)} />}
       </div>
     </AppLayout>
   );
